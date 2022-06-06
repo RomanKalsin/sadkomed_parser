16d80d86fe6dc92a101411a8882f69c20d1cb94a
RomanKalsin <karen52rus@gmail.com>
2022-06-06 17:25:49 +0300

    inst.sh

--- inst.sh
+++ inst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt update -y
-apt install curl python3-venv -y
-curl -sSL https://install.python-poetry.org | python3 -
+apt install curl python3-venv python3-pip -y
 export PATH="/root/.local/bin:$PATH"
+curl -sSL https://install.python-poetry.org | python3 -
